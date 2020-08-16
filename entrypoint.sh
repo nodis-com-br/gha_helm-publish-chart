9667ac33ff70c164f54c3dde4b7fc8fce3aae761
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-08-16 02:26:47 -0300

    Fixed env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-helm repo add chartmuseum https://${CHART_REPOSITORY_USER}:${CHART_REPOSITORY_PASS}@{CHART_REPOSITORY_HOST}
+helm repo add chartmuseum https://${NODIS_CHART_REPOSITORY_USER}:${NODIS_CHART_REPOSITORY_PASS}@{NODIS_CHART_REPOSITORY_HOST}
 
 helm package .
 
